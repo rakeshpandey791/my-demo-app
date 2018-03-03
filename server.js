8fb42498c42853fe2d63371dbe7b8d9446c9895a
rpandey <rpandey@wdtablesystems.com>
2018-03-03 10:21:22 +0530

    add server.js

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/disc"));
+app.get("/", (req, res) => {
+  res.send("Hello Guys");
+});
 app.listen(process.env.PORT || 8080);
